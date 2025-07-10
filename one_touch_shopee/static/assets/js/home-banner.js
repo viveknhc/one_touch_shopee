55df5e76c41155edc37c3d30f59422ff32598d4b
viveknhc <viveknhc@gmail.com>
2025-07-10 20:38:41 +0530

    03/07/25

--- one_touch_shopee/static/assets/js/home-banner.js
+++ one_touch_shopee/static/assets/js/home-banner.js
@@ -20,10 +20,51 @@ var swiper = new Swiper(".productSwiper", {
         prevEl: ".swiper-button-prev",
     },
   
-    // autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: false,
-    // },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+    loop: true,
+
+
+    breakpoints: {
+
+        200: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        767: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+
+        999: {
+            slidesPerView: 6,
+            spaceBetween: 30
+        },
+        1024: {
+            slidesPerView: 6,
+            spaceBetween: 30
+        }
+    }
+});
+
+var swiper = new Swiper(".brandSwiper", {
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+  
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
 
     loop: true,
 
